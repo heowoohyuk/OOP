5336ce0ba210131593d14fdc277ea8e2645eccd9
heowoohyuk <hwh3858@naver.com>
2019-11-19 17:19:53 +0900

    10-2주차 midterm

---
+++ midterm/smart_obj-hw/student.cpp
@@ -0,0 +1,29 @@
+#include "student.h"
+
+student::student() {
+	this->name = nullptr;
+	this->student_id = -1;
+}
+
+student::student(char* name) {
+	this->name= name;
+	this->student_id=-1;
+}
+
+student::student(int student_id) {
+	this->name= nullptr;
+	this->student_id=student_id;
+}
+
+student::student(char *name, int student_id) {
+     this->name=name;
+	 this->student_id=student_id;
+}
+
+int student::get_student_id() {
+    return this->student_id;
+}
+
+int student::get_id() {
+    return 1;
+}
